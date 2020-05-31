dde86c64b48acc681d60ceffc35ebecdcb336286
Benjamin Kniaz <benjaminkniaz@Benjamins-MBP.fios-router.home>
2020-05-31 17:29:33 -0400

    Updates

---
+++ precache-manifest.030194f1728970cc7fdbe4bbd9ba8a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6aaf89b96b853a0630b2de2d4062e4",
+    "url": "/madlibs/index.html"
+  },
+  {
+    "revision": "6dab498a972eea11ea4c",
+    "url": "/madlibs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "27e0bbc149204b035c0c",
+    "url": "/madlibs/static/css/main.abef8036.chunk.css"
+  },
+  {
+    "revision": "6dab498a972eea11ea4c",
+    "url": "/madlibs/static/js/2.196f6975.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/madlibs/static/js/2.196f6975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e0bbc149204b035c0c",
+    "url": "/madlibs/static/js/main.cc7fa884.chunk.js"
+  },
+  {
+    "revision": "fed09eb22e57d0d2d07a",
+    "url": "/madlibs/static/js/runtime-main.f36effcc.js"
+  }
+]);
