677439af22865f5c3978710d254c8a39c4ab7d96
Ben Kniaz <benjaminkniaz@gmail.com>
2020-07-14 17:16:41 -0400

    modified styling; changed url and copyright name

--- src/App.js
+++ src/App.js
@@ -5,42 +5,46 @@ import FillTheBlank from "./components/FillTheBlank";
 import ParagraphWord from "./components/ParagraphWord";
 import "./App.scss";
 
-const example = `Use square brackets to create blanks in your Mad Lib. You can [verb] text here from any article or [noun] you wish!`;
+const example = `Type the story here. Use square brackets to create blanks in the paragraph. For example: "Johnny [verb] a [adjective] pie because it was [holiday]."`;
 
 export default function App() {
-  const [madLibsText, setMadLibsText] = useState(example);
-  const [printableArea, setPrintableArea] = useState(<div />);
-  useEffect(createPrintableText, [madLibsText]);
+    const [madLibsText, setMadLibsText] = useState(example);
+    const [printableArea, setPrintableArea] = useState(<div />);
+    useEffect(createPrintableText, [madLibsText]);
 
-  function createPrintableText() {
-    var paragraph = [];
+    function createPrintableText() {
+        var paragraph = [];
 
-    const words = ParserFuncs.getParagraphWords(madLibsText);
+        const words = ParserFuncs.getParagraphWords(madLibsText);
 
-    words.forEach((word, index) => {
-      const fillBlankInstruction = ParserFuncs.getFillBlankInstruction(word);
-      if (fillBlankInstruction !== "") {
-        const adjacentChars = ParserFuncs.getAdjacentFillBlankCharacters(word);
-        paragraph.push(
-          <FillTheBlank
-            key={index}
-            fillBlankInstruction={fillBlankInstruction}
-            adjacentFillBlankCharacters={adjacentChars}
-          />
-        );
-      } else {
-        paragraph.push(<ParagraphWord key={index} word={word} />);
-      }
-    });
+        words.forEach((word, index) => {
+            const fillBlankInstruction = ParserFuncs.getFillBlankInstruction(
+                word
+            );
+            if (fillBlankInstruction !== "") {
+                const adjacentChars = ParserFuncs.getAdjacentFillBlankCharacters(
+                    word
+                );
+                paragraph.push(
+                    <FillTheBlank
+                        key={index}
+                        fillBlankInstruction={fillBlankInstruction}
+                        adjacentFillBlankCharacters={adjacentChars}
+                    />
+                );
+            } else {
+                paragraph.push(<ParagraphWord key={index} word={word} />);
+            }
+        });
 
-    setPrintableArea(paragraph);
-  }
+        setPrintableArea(paragraph);
+    }
 
-  return (
-    <Layout
-      printableArea={printableArea}
-      madLibsText={madLibsText}
-      setMadLibsText={setMadLibsText}
-    />
-  );
+    return (
+        <Layout
+            printableArea={printableArea}
+            madLibsText={madLibsText}
+            setMadLibsText={setMadLibsText}
+        />
+    );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,34 +2,34 @@ import React from "react";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 export default ({ printableArea, madLibsText, setMadLibsText }) => (
-	<Container>
-		<Row>
-			<Col>
-				<h1>Printable Cut-and-Paste Imitation Mad Libs Maker!</h1>
-				<Form className="mad-libs-entry-area">
-					<Form.Group>
-						<Form.Control
-							as="textarea"
-							rows="6"
-							className="mad-libs-create-text"
-							value={madLibsText}
-							onChange={(e) => setMadLibsText(e.target.value)}
-						/>
-					</Form.Group>
-				</Form>
+    <Container>
+        <Row>
+            <Col>
+                <h1>Printable, Cut-and-Paste "Mad Libs"</h1>
+                <Form className="mad-libs-entry-area">
+                    <Form.Group>
+                        <Form.Control
+                            as="textarea"
+                            rows="6"
+                            className="mad-libs-create-text"
+                            value={madLibsText}
+                            onChange={(e) => setMadLibsText(e.target.value)}
+                        />
+                    </Form.Group>
+                </Form>
 
-				<div className="printable-area">{printableArea}</div>
+                <div className="printable-area">{printableArea}</div>
 
-				<Button
-					onClick={window.print}
-					variant="dark"
-					className="print-btn"
-				>
-					Print me!
-				</Button>
+                <Button
+                    onClick={window.print}
+                    variant="dark"
+                    className="print-btn"
+                >
+                    Print me!
+                </Button>
 
-				<p className="credit">made by bkniaz</p>
-			</Col>
-		</Row>
-	</Container>
+                <p className="credit">by Ben Kniaz</p>
+            </Col>
+        </Row>
+    </Container>
 );
